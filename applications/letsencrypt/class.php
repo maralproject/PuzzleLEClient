435f6fadc0251348030f2a85a9279b562f5350fd
Rifqi <rifweb.android@gmail.com>
2019-02-26 17:54:46 +0700

    update

--- applications/letsencrypt/class.php
+++ applications/letsencrypt/class.php
@@ -29,7 +29,7 @@ class ACME{
 	public static function recursiveRemoveDirectory($directory)
 	{
 		foreach(glob("{$directory}/*") as $file){
-			if(is_dir($file)) recursiveRemoveDirectory($file);
+			if(is_dir($file)) self::recursiveRemoveDirectory($file);
 			else unlink($file);
 		}
 		rmdir($directory);
